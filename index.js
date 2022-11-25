800a280dc5f6026272dc43b1efbc3ecaa12c74b3
Grandman7777 <kokaiya7777@gmail.com>
2022-11-25 20:35:09 +0200

    CardUser

--- index.js
+++ index.js
@@ -1,6 +1,64 @@
-const div = document.createElement('div');
-const img = document.createElement('img');
-img.setAttribute('src','./Images\6uGglXbkoV.jpg');
-div.append(img);
+const telephon = {
+    name: 'Samsung',
+    avatar: './',
+    description: 'DichDich',
+    price: '100$'
+};
 
-document.body.append(div)
+const telephoneArray = [
+    {
+        name: 'Samsung',
+    avatar: './',
+    description: 'DichDich',
+    price: '100$'
+    },
+    {
+    name: 'Nokia',
+    avatar: './',
+    description: 'DichDich',
+    price: '50$'
+    },
+    {
+        name: 'Xiaomi',
+        avatar: './',
+        description: 'DichDich',
+        price: '150$'
+        }
+]
+
+for(const tel of telephoneArray) {
+    console.log(`pfhone ${tel.name} prices ${tel.price}`)
+}
+///////////////////////////////////////////////////////////////
+
+const userData = {
+id: 1,
+name: 'John',
+discription: 'awagagfaf',
+profilePictures: 'asfewagwegssf'
+};
+
+const root = document.querySelector('#root')
+
+function creatrCard(user) {
+const article = document.createElement('article');
+article.classList.add('card-wrapper');
+
+const img = document.createElement('img');
+img.classList.add('card-image');
+img.setAttribute('src', user.profilePictures);
+img.setAttribute('alt',user.name);
+
+const h2 = document.createElement('h2');
+h2.classList.add('username')
+h2.append(user.name);
+
+const p = document.createElement('p');
+p.classList.add('discription');
+p.append(user.discription);
+
+article.append(img, h2, p);
+root.append(article);
+}
+
+creatrCard(userData);
