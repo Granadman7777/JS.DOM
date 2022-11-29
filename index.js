0055fa15deaad37ce7e30ae0781bd825fa5d6cfd
Grandman7777 <kokaiya7777@gmail.com>
2022-11-29 18:26:24 +0200

    ToDoList

--- index.js
+++ index.js
@@ -1,64 +1,22 @@
-const telephon = {
-    name: 'Samsung',
-    avatar: './',
-    description: 'DichDich',
-    price: '100$'
-};
+const form = document.getElementById('form');
+const root = document.getElementById('root');
 
-const telephoneArray = [
-    {
-        name: 'Samsung',
-    avatar: './',
-    description: 'DichDich',
-    price: '100$'
-    },
-    {
-    name: 'Nokia',
-    avatar: './',
-    description: 'DichDich',
-    price: '50$'
-    },
-    {
-        name: 'Xiaomi',
-        avatar: './',
-        description: 'DichDich',
-        price: '150$'
-        }
-]
+const state = []; // Тут будут Таски 
+ 
+form.addEventListener('submit',addItem);
 
-for(const tel of telephoneArray) {
-    console.log(`pfhone ${tel.name} prices ${tel.price}`)
-}
-///////////////////////////////////////////////////////////////
-
-const userData = {
-id: 1,
-name: 'John',
-discription: 'awagagfaf',
-profilePictures: 'asfewagwegssf'
-};
-
-const root = document.querySelector('#root')
-
-function creatrCard(user) {
-const article = document.createElement('article');
-article.classList.add('card-wrapper');
-
-const img = document.createElement('img');
-img.classList.add('card-image');
-img.setAttribute('src', user.profilePictures);
-img.setAttribute('alt',user.name);
-
-const h2 = document.createElement('h2');
-h2.classList.add('username')
-h2.append(user.name);
-
-const p = document.createElement('p');
-p.classList.add('discription');
-p.append(user.discription);
-
-article.append(img, h2, p);
-root.append(article);
+function addItem(event) {
+    event.preventDefault();
+    const {target: {textInput: {value}}} = event;
+    state.push(value);
+    const li = createItem(value);
+    root.append(li);
+    target.reset()
 }
 
-creatrCard(userData);
+function createItem(value) {
+    const li = document.createElement('li');
+    li.append(value);
+    li.classList.add('item');
+    return li;
+}
