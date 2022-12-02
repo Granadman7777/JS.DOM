01ebce1088afb99e9b5730e887d57a767e0332e4
Grandman7777 <kokaiya7777@gmail.com>
2022-12-02 18:19:46 +0200

    Then

--- index.js
+++ index.js
@@ -1,19 +1,15 @@
-const box = document.querySelector('#box');
+const p = new Promise(function(resolve, reject) {
+setTimeout(() => {
+    resolve('it is time');
+}, 15000)
+})
 
-box.addEventListener('click', boxClickHandler);
 
-function boxClickHandler(event) {
-    const input = documents.createElement('input');
-    input.addEventListener('keydown', inputKeyHandler);
-    const parent = event.target.parentNode;
-    parent.append(input)
-}
-
-function inputKeyHandler({target, target: {value,previosElementSibling}}) {
-if(event.keyCode === 13) {
-    // передать данные элементу (box)
-    previosElementSibling.textContent = value;
-    // Удаляем input при помощи keyCode 
-    target.remove();
-}
-}
+p.then(
+    (data) => {
+        console.log(data)
+    },
+    (err) => {
+        console.log(err)
+    }
+);
