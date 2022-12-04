5245c6bda0309cb810452d5e5dc5fbaedf3d2955
Grandman7777 <kokaiya7777@gmail.com>
2022-12-04 15:04:51 +0200

    Figure

--- index.js
+++ index.js
@@ -1,15 +1,32 @@
-const p = new Promise(function(resolve, reject) {
-setTimeout(() => {
-    resolve('it is time');
-}, 15000)
-})
-
-
-p.then(
-    (data) => {
-        console.log(data)
-    },
-    (err) => {
-        console.log(err)
+class Figure {
+    constructor(sideQuantity) {
+        this.sideQuantity = sideQuantity;
     }
-);
+
+    getArea() {
+
+    }
+}
+
+class Triangle extends Figure {
+    constructor(a, b, angle) {
+        super(3);
+        this.a = a;
+        this.b = b;
+        this.angle = angle;
+    }
+
+    getArea() {
+        return this.a * this.b * Math.sin(this.angle);
+    }
+}
+
+class Square extends Figure {
+    constructor(a) {
+        super(4);
+        this.a; 
+    }
+    getArea() {
+        return this.a * this.a ;
+    }
+}
